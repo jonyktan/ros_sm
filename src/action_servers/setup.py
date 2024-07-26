17a000ba88a948d9a0d2a26f5202adb83b2483b8
Jon <jon.yktan03@gmail.com>
2024-07-26 19:22:40 +0800

    action server as ros 2 executable

--- src/action_servers/setup.py
+++ src/action_servers/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'simple_action_server = action_servers.simple_action_server:main'
         ],
     },
 )
