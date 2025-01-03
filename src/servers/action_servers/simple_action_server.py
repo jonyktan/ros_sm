c80720d02dd9b9c6fae61e9683fc6b4d37613dbb
jonyktan <jon.yktan03@gmail.com>
2025-01-03 16:01:02 +0800

    [feat] add custom cancel_callback to action server to allow goal cancel

--- src/servers/action_servers/simple_action_server.py
+++ src/servers/action_servers/simple_action_server.py
@@ -1,7 +1,7 @@
 import time
 
 import rclpy
-from rclpy.action import ActionServer
+from rclpy.action import ActionServer, CancelResponse
 from rclpy.node import Node
 
 from custom_interfaces.action import Simpleactiontype
@@ -13,9 +13,11 @@ class SimpleActionServer(Node):
         super().__init__('simple_action_node')
         self._action_server = ActionServer(
             self,
-            Simpleactiontype,
-            'simple_action_name',
-            self.execute_callback)
+            action_type=Simpleactiontype,
+            action_name='simple_action_name',
+            execute_callback=self.execute_callback,
+            cancel_callback=self.cancel_callback
+            )
 
     # From sample Fibonacci action server
     def execute_callback(self, goal_handle):
@@ -37,6 +39,19 @@ class SimpleActionServer(Node):
         result.simple_result = feedback_msg.simple_feedback
         self.get_logger().info(f'Goal ({goal_handle.request.simple_request}) reached, result: {result.simple_result}')
         return result
+
+    def cancel_callback(self, goal_handle):
+        """Custom callback to allow or reject goal cancellation. (Default ROS 2 action servers reject all cancellations.)"""
+
+        self.get_logger().info(f"Cancel request received.")
+
+        # Example logic: Allow cancellation only if the order is above a threshold
+        if goal_handle.request.order >=10:
+            self.get_logger().info(f"Cancel request accepted.")
+            return CancelResponse.ACCEPT
+        
+        self.get_logger().info(f"Cancel request rejected as order {goal_handle.request.order} was less than 10.")
+        return CancelResponse.REJECT
 
 
 def main(args=None):
